0135067e3301f6d5bec1c5710e64f69532a9acfd
mahmoud hossni <81756795+mahmoudhossni147852@users.noreply.github.com>
2023-08-28 20:58:44 +0300

    Update first-ec2.tf

--- first-ec2.tf
+++ first-ec2.tf
@@ -1,8 +1,6 @@
 #user credention in AWS
 provider "aws" {
   region     = "us-east-2"
-  access_key = "AKIAUTE6TA2QIQ7KJH4R"
-  secret_key = "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"
 } 
 
 resource "aws_instance" "prod" {
